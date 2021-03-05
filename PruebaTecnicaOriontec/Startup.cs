1d6702d7f8f7e6c04b45a0e5d79768e0ff576e26
Randy Dominguez <32862344+SoyRandyDominguez@users.noreply.github.com>
2021-03-05 00:49:11 -0400

    Some initial configurations

--- PruebaTecnicaOriontec/Startup.cs
+++ PruebaTecnicaOriontec/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PruebaTecnicaOriontec.Helpers;
 
 namespace PruebaTecnicaOriontec
 {
@@ -26,6 +28,14 @@ namespace PruebaTecnicaOriontec
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext.AppContext>(options =>
+                      options.UseSqlServer(
+                          Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ICommandText, CommandText>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +55,13 @@ namespace PruebaTecnicaOriontec
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
