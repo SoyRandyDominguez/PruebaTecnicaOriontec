e7bb993412573f3c70a5abb5ca646f49a398ca11
Randy Dominguez <32862344+SoyRandyDominguez@users.noreply.github.com>
2021-03-05 00:41:19 -0400

    Creando modelos

---
+++ PruebaTecnicaOriontec/Models/DireccionCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaOriontec.Models
+{
+    public class DireccionCliente
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public string Direccion { get; set; }
+    }
+}
